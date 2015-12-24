437ee6cbec7a31d602b086c3b2dfe99d6d5dc9fc
Kathleen Kent <lashkath@gmail.com>
2015-12-23 16:14:06 -0800

    initial commit

---
+++ simple-shop-child/footer.php
@@ -0,0 +1,13 @@
+		<!-- Footer -->
+		<footer id="footer">
+
+			<section class="copyright">
+				<p class="copyright-message">
+					&copy Copyright <a href="http://www.lynnsvintage.com">Lynn's Vintage Shop</a> <?php echo date("Y") ?>
+				</p>
+			</section>
+		</footer>
+
+	<?php wp_footer(); ?>
+	</body>
+</html>
